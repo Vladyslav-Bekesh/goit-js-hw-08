9fd46f9192ec06b9c93ddbc7d741c4cdb58c79b1
Vladyslav <dyadyavadya308@gmail.com>
2023-03-30 18:21:41 +0300

    library added + tested

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,43 @@
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+//! gallery find
+const gallery = document.querySelector('.gallery');
+
+//! markup create
+const galleryMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          alt="${description}"
+        />
+      </a>
+    </li>`
+  )
+  .join('');
+
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+//! event listener
+
+gallery.addEventListener('click', openBigImage);
+
+//! functions
+
+function openBigImage(event) {
+  event.preventDefault();
+  const element = new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+    captionsData: 'alt',
+  });
+}
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,9 @@
+import Player from '@vimeo/player';
 
+//! PLAYER ---------
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', ({seconds}) => {
+  console.log(seconds);
+}); 
