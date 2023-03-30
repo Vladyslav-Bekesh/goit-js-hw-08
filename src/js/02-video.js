903e363318bf442aa226f6a7cd09bcd9c2d19dc3
Vladyslav <dyadyavadya308@gmail.com>
2023-03-30 21:34:52 +0300

    task 2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,9 +1,21 @@
+//! IMPORT LIBRARIES ---------
 import Player from '@vimeo/player';
+import _ from 'lodash';
+
 
 //! PLAYER ---------
+//* refs
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('timeupdate', ({seconds}) => {
-  console.log(seconds);
-}); 
+//*player current time
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
+//*player current time
+player.on(
+  'timeupdate',
+  _.throttle(({seconds}) => {
+    //throttle body
+    localStorage.setItem('videoplayer-current-time', seconds);
+  }, 1000)
+);
